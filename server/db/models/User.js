9f6d00b28df3cdb3107424fe3760e8952829c8fe
Inderprit Singh <singhinderprit1992@gmail.com>
2022-04-06 00:15:31 -0400

    dotenv stuff

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -2,108 +2,111 @@ const Sequelize = require('sequelize');
 const { db } = require('../db');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-require('dotenv').config();
+// require('dotenv').config();
 
 //define your model
 
 const User = db.define('user', {
-  isAdmin: {
-    type: Sequelize.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-    validate: {
-      notEmpty: true,
+    isAdmin: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+        validate: {
+            notEmpty: true,
+        },
     },
-  },
-  role: {
-    type: Sequelize.ENUM(['AUTHENTICATED', 'GUEST']),
-    allowNull: false,
-    defaultValue: 'GUEST',
-    validate: {
-      notEmpty: true,
+    role: {
+        type: Sequelize.ENUM(['AUTHENTICATED', 'GUEST']),
+        allowNull: false,
+        defaultValue: 'GUEST',
+        validate: {
+            notEmpty: true,
+        },
     },
-  },
-  first: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
+    first: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
     },
-  },
-  last: {
-    type: Sequelize.STRING,
-  },
-  fullName: {
-    type: Sequelize.VIRTUAL,
-    defaultValue: 'guest',
-    get() {
-      if (this.last) {
-        return `${this.first} ${this.last}`;
-      } else {
-        return `${this.first}`;
-      }
+    last: {
+        type: Sequelize.STRING,
     },
-  },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    defaultValue: 'guest_pw',
-    validate: {
-      notEmpty: true,
+    fullName: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: 'guest',
+        get() {
+            if (this.last) {
+                return `${this.first} ${this.last}`;
+            } else {
+                return `${this.first}`;
+            }
+        },
     },
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-    defaultValue: 'guestEmail@gmail.com',
-    validate: {
-      notEmpty: true,
-      isEmail: true,
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'guest_pw',
+        validate: {
+            notEmpty: true,
+        },
     },
-  },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        defaultValue: 'guestEmail@gmail.com',
+        validate: {
+            notEmpty: true,
+            isEmail: true,
+        },
+    },
 });
 
 User.authenticate = async function ({ email, password }) {
-  const user = await User.findOne({
-    where: {
-      email,
-    },
-  });
-  if (user && (await bcrypt.compare(password, user.password))) {
-    const token = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET);
-    return token;
-  }
-  const error = Error('bad credentials');
-  error.status = 401;
-  throw error;
+    const user = await User.findOne({
+        where: {
+            email,
+        },
+    });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+            { id: user.id },
+            process.env.ACCESS_TOKEN_SECRET
+        );
+        return token;
+    }
+    const error = Error('bad credentials');
+    error.status = 401;
+    throw error;
 };
 
 User.byToken = async function (token) {
-  try {
-    const { id } = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findByPk(id);
-    if (user) return user;
-    const error = Error('bad credentials');
-    error.status = 401;
-    throw error;
-  } catch (ex) {
-    const error = Error('bad credentials');
-    error.status = 401;
-    throw error;
-  }
+    try {
+        const { id } = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findByPk(id);
+        if (user) return user;
+        const error = Error('bad credentials');
+        error.status = 401;
+        throw error;
+    } catch (ex) {
+        const error = Error('bad credentials');
+        error.status = 401;
+        throw error;
+    }
 };
 
 User.addHook('beforeCreate', async (user) => {
-  //if (user.changed('password')) { ////Arjan comment: i commented this out to check logging in with admin users, uncomment this when we deploy to PROD*******
-  user.password = await bcrypt.hash(user.password, 5);
-  //}
+    //if (user.changed('password')) { ////Arjan comment: i commented this out to check logging in with admin users, uncomment this when we deploy to PROD*******
+    user.password = await bcrypt.hash(user.password, 5);
+    //}
 });
 
 User.addHook('beforeUpdate', async (user) => {
-  //if (user.changed('password')) { ////Arjan comment: i commented this out to check logging in with admin users, uncomment this when we deploy to PROD*******
-  user.password = await bcrypt.hash(user.password, 5);
-  //}
+    //if (user.changed('password')) { ////Arjan comment: i commented this out to check logging in with admin users, uncomment this when we deploy to PROD*******
+    user.password = await bcrypt.hash(user.password, 5);
+    //}
 });
 
 //define any class or instance methods
--- server/index.js
+++ server/index.js
@@ -1,80 +1,80 @@
 //add unique true on user model????
-const User = require("./db/models/User");
-const express = require("express");
+const User = require('./db/models/User');
+const express = require('express');
 const app = express();
-const path = require("path");
-const morgan = require("morgan");
-const { init } = require("./db/index");
+const path = require('path');
+const morgan = require('morgan');
+const { init } = require('./db/index');
 init();
-require("dotenv").config(); // I DONT THINK WE NEED THIS IN MULTIPLE PLACES TBH. JUST HAVING IT IN USER FOR SOME REASON IS GOOD ENOUGH? DOUBLE CHECK
+//require("dotenv").config(); // I DONT THINK WE NEED THIS IN MULTIPLE PLACES TBH. JUST HAVING IT IN USER FOR SOME REASON IS GOOD ENOUGH? DOUBLE CHECK
 const port = process.env.PORT || 9000;
-const axios = require("axios");
-const jwt = require("jsonwebtoken");
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static(path.join(__dirname, "..", "public")));
+app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 
-app.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-app.get("/github/callback", async (req, res, next) => {
-  try {
-    let response = await axios.post(
-      "https://github.com/login/oauth/access_token",
-      {
-        code: req.query.code,
-        client_id: process.env.client_id,
-        client_secret: process.env.client_secret,
-      },
-      {
-        headers: {
-          accept: "application/json",
-        },
-      }
-    );
-    const data = response.data;
-    if (data.error) {
-      const error = Error(data.error);
-      error.status = 401;
-      throw error;
-    }
-    //third step in the docs, this step requires you to send the authorization token you received as the header, you get back access_token
-    //this is a get request so there is no body?
-    //this needs to come in the format Authorization: token OAUTH-TOKEN"
-    //should now get information about myself
-    response = await axios.get("https://api.github.com/user", {
-      headers: {
-        authorization: `token ${data.access_token}`,
-      },
-    });
+app.get('/github/callback', async (req, res, next) => {
+    try {
+        let response = await axios.post(
+            'https://github.com/login/oauth/access_token',
+            {
+                code: req.query.code,
+                client_id: process.env.client_id,
+                client_secret: process.env.client_secret,
+            },
+            {
+                headers: {
+                    accept: 'application/json',
+                },
+            }
+        );
+        const data = response.data;
+        if (data.error) {
+            const error = Error(data.error);
+            error.status = 401;
+            throw error;
+        }
+        //third step in the docs, this step requires you to send the authorization token you received as the header, you get back access_token
+        //this is a get request so there is no body?
+        //this needs to come in the format Authorization: token OAUTH-TOKEN"
+        //should now get information about myself
+        response = await axios.get('https://api.github.com/user', {
+            headers: {
+                authorization: `token ${data.access_token}`,
+            },
+        });
 
-    //NOTE: we may not be able to use email in case someone has a null email on github
-    const { email, login } = response.data;
+        //NOTE: we may not be able to use email in case someone has a null email on github
+        const { email, login } = response.data;
 
-    let user = await User.findOne({
-      where: {
-        email: email,
-      },
-    });
-    //i am using these are hard coded tests for now below
-    const first = login;
-    const password = "guest_pw";
-    const role = "AUTHENTICATED";
-    const isAdmin = false;
-    //if user doesn't exist yet
-    if (!user) {
-      user = await User.create({ isAdmin, first, role, password, email });
-    }
+        let user = await User.findOne({
+            where: {
+                email: email,
+            },
+        });
+        //i am using these are hard coded tests for now below
+        const first = login;
+        const password = 'guest_pw';
+        const role = 'AUTHENTICATED';
+        const isAdmin = false;
+        //if user doesn't exist yet
+        if (!user) {
+            user = await User.create({ isAdmin, first, role, password, email });
+        }
 
-    const jwtToken = await jwt.sign(
-      { id: user.id },
-      process.env.ACCESS_TOKEN_SECRET
-    );
+        const jwtToken = await jwt.sign(
+            { id: user.id },
+            process.env.ACCESS_TOKEN_SECRET
+        );
 
-    res.send(`<html>
+        res.send(`<html>
       <head>
       <script>
       window.localStorage.setItem('guest', 'false');
@@ -83,28 +83,28 @@ app.get("/github/callback", async (req, res, next) => {
       </script>
       </head>
     </html>`);
-  } catch (ex) {
-    next(ex);
-  }
+    } catch (ex) {
+        next(ex);
+    }
 });
 
 //some middleware
 //we are getting jwt from github?
 app.use((err, req, res, next) => {
-  res.status(err.status).send({ error: err.message });
+    res.status(err.status).send({ error: err.message });
 });
 
-app.use("/api/products/", require("./api/routes/productRoute"));
-app.use("/api/users/", require("./api/routes/userRoute"));
-app.use("/api/orders/", require("./api/routes/orderRoute"));
-app.use("/api/reviews/", require("./api/routes/reviewRoute"));
-app.use("/api/categories", require("./api/routes/categoryRoute"));
-app.use("/api/auth", require("./api/routes/authRoute"));
-app.use("/api/order_products/", require("./api/routes/orderProductRoute"));
+app.use('/api/products/', require('./api/routes/productRoute'));
+app.use('/api/users/', require('./api/routes/userRoute'));
+app.use('/api/orders/', require('./api/routes/orderRoute'));
+app.use('/api/reviews/', require('./api/routes/reviewRoute'));
+app.use('/api/categories', require('./api/routes/categoryRoute'));
+app.use('/api/auth', require('./api/routes/authRoute'));
+app.use('/api/order_products/', require('./api/routes/orderProductRoute'));
 
 //500 handler
 app.use((error, req, res, next) => {
-  res.status(500).send(error);
+    res.status(500).send(error);
 });
 
 app.listen(port, () => console.log(`listening on port: ${port}`));
